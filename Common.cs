c54d91724757c2dbeeedde454213c9881dcde35a
monasandesh <37885929+monasandesh@users.noreply.github.com>
2018-03-28 20:01:42 -0400

    Add files via upload

---
+++ Common.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Surrogacy.Entity;
+
+namespace Surrogacy.Util
+{
+    #region Enum Properties
+
+    public enum EntityState
+    {
+        View,
+        Save,
+        Edit,
+        Delete,
+        None
+    }
+
+    public enum MachineType
+    {
+        Desktop,
+        Mobile,
+        Tablet
+    }
+
+    public enum ContactType
+    {
+        Email,
+        Phone,
+        Fax
+    }
+    public enum ResponseType
+    {
+        Warning,
+        Error,
+        Information,
+        Success
+    }
+
+    public enum InputType
+    {
+        UserName,
+        Password,
+        Text,
+        Number
+    }
+
+    public enum DropDownControlType
+    {
+        Html,
+        Asp
+    }
+
+    public enum DropDownControlFilterBy
+    {
+        Category,
+        ParentCategory
+    }
+
+    public enum CustomFileType
+    {
+       IdProof,
+       Pic,
+       FamilyPic
+    }
+
+    #endregion Enum Properties    
+}
